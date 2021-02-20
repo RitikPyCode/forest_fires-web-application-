9a90957a53748902bd279d316d3fc7b88fdaf030
Ritik kumar tiwari <69500530+RitikPyCode@users.noreply.github.com>
2021-02-21 00:24:30 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv(r"https://raw.githubusercontent.com/nachi-hebbar/Streamlit-ML-Web-App/master/Forest_fire.csv")
+data = np.array(data)
+
+X = data[1:, 1:-1]
+y = data[1:, -1]
+y = y.astype('int')
+X = X.astype('int')
+# print(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+log_reg = LogisticRegression()
+
+
+log_reg.fit(X_train, y_train)
+
+pickle.dump(log_reg,open('model.pkl','wb'))
